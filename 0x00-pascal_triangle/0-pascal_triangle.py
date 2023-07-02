ae477f8c1679314928f5c3a2878e73255b8dacdd
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-02 20:13:27 +0100

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+def pascal_triangle(n):
+    """pascal's triangle"""
+    if n <= 0:
+        return []
+    triangle = []    
+    for i in range(n):
+        array = [1] * (i + 1)
+        for j in range(1, len(array) - 1):
+            prev = triangle[i - 1]
+            array[j] = prev[j - 1] + prev[j]
+        triangle.append(array)
+    return arraylist
