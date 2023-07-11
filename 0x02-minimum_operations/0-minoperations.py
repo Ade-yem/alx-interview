96d08460cce4034726c7a1afc1606c29984ad3d8
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-12 00:11:00 +0100

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,15 +5,14 @@
 def minOperations(n):
     """calculates the fewest number of operations needed
     to result in exactly n H characters in the file"""
-    init = "H"
-    ops = 1
-    if n > 1:
-        for _ in range(n):
-            if (ops % 3 == 0):
-                init += init
-            else:
-                init += "H"
-            ops += 1
-            if len(init) >= n:
-                return ops
-    return 0
+    ops = 0
+    div = 2
+    if n < 1:
+        return 0
+    while n > 1:
+        while n % div == 0:
+            ops += div
+            n //= div
+        div += 1
+    return ops
+ 
