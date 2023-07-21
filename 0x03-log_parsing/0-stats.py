53ae76c5b5b150c6e69772fd92dd58158c86ca4a
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-21 19:17:14 +0100

    fixed the error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,12 +3,6 @@
 
 import sys
 
-file_size = 0
-count = 1
-codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-stats = {"200": 0, "301": 0, "400": 0, "401": 0,
-         "403": 0, "404": 0, "405": 0, "500": 0}
-
 
 def print_stats(file_size, stats):
     """prints the stats"""
@@ -19,18 +13,29 @@ def print_stats(file_size, stats):
         stats[k] = 0
 
 
-try:
-    for line in sys.stdin:
-        line = line.split()
-        if len(line) != 9:
-            continue
-        code = line[7]
-        if code in codes:
-            stats[code] += 1
-        file_size += int(line[8])
-        if count % 10 == 0:
-            print_stats(file_size, stats)
-        count += 1
+def main():
+    """entry point"""
+    file_size = 0
+    count = 1
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {"200": 0, "301": 0, "400": 0, "401": 0,
+             "403": 0, "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            line = line.split()
+            if len(line) != 9:
+                continue
+            code = line[7]
+            if code in codes:
+                stats[code] += 1
+            file_size += int(line[8])
+            if count % 10 == 0:
+                print_stats(file_size, stats)
+            count += 1
 
-except (IndexError, KeyboardInterrupt) as e:
-    print_stats(file_size, stats)
+    except (IndexError, KeyboardInterrupt) as e:
+        print_stats(file_size, stats)
+
+
+if __name__ == "__main__":
+    main()
