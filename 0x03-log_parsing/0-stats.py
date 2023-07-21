03c2adb57f84abe097ee4ebc4690342d601ddabc
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-21 20:00:04 +0100

    update stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
 
-if __name__ == "__main__":
-    import sys
+import sys
 
-    def print_stats(file_size, stats):
-        """prints the stats"""
-        print(f"File size: {file_size}")
-        for k, v in sorted(stats.items()):
-            if stats[k] > 0:
-                print(f"{k}: {v}")
-            stats[k] = 0
+def print_stats(file_size, stats):
+    """prints the stats"""
+    print(f"File size: {file_size}")
+    for k, v in sorted(stats.items()):
+        if stats[k] > 0:
+            print(f"{k}: {v}")
+    for k in stats.keys():
+        stats[k] = 0
 
-    file_size = 0
-    count = 0
-    stats = {"200": 0, "301": 0, "400": 0, "401": 0,
-             "403": 0, "404": 0, "405": 0, "500": 0}
-    try:
-        for line in sys.stdin:
-            line = line.split()
-            if len(line) != 9:
-                continue
-            code = line[-2]
-            if code in stats.keys():
-                stats[code] += 1
-            file_size += int(line[-1])
-            count += 1
-            if count > 0 and count % 10 == 0:
-                print_stats(file_size, stats)
-    except (IndexError, KeyboardInterrupt) as e:
-        print_stats(file_size, stats)
+file_size = 0
+count = 0
+stats = {"200": 0, "301": 0, "400": 0, "401": 0,
+            "403": 0, "404": 0, "405": 0, "500": 0}
+try:
+    for line in sys.stdin:
+        line = line.split()
+        if len(line) != 9:
+            continue
+        code = line[-2]
+        if code in stats.keys():
+            stats[code] += 1
+        file_size += int(line[-1])
+        count += 1
+        if count > 0 and count % 10 == 0:
+            print_stats(file_size, stats)
+except (IndexError, KeyboardInterrupt) as e:
+    print_stats(file_size, stats)
