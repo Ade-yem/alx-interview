98983945771f56f2b72bff12b4901f7add31e273
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-07-21 20:05:54 +0100

    fixed the error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,19 +3,19 @@
 
 import sys
 
+
 def print_stats(file_size, stats):
     """prints the stats"""
     print(f"File size: {file_size}")
     for k, v in sorted(stats.items()):
         if stats[k] > 0:
             print(f"{k}: {v}")
-    for k in stats.keys():
-        stats[k] = 0
+
 
 file_size = 0
 count = 0
 stats = {"200": 0, "301": 0, "400": 0, "401": 0,
-            "403": 0, "404": 0, "405": 0, "500": 0}
+         "403": 0, "404": 0, "405": 0, "500": 0}
 try:
     for line in sys.stdin:
         line = line.split()
@@ -26,7 +26,8 @@ try:
             stats[code] += 1
         file_size += int(line[-1])
         count += 1
-        if count > 0 and count % 10 == 0:
+        if count % 10 == 0:
             print_stats(file_size, stats)
+            count = 0
 except (IndexError, KeyboardInterrupt) as e:
     print_stats(file_size, stats)
