6d53e0e75fd0e255fee90c9c3723c20bb1726b79
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-07-29 09:12:22 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,19 @@
 
 def validUTF8(data):
     """UTF-8 Validator"""
-    lis = [i for i in range(0, 128)]
-    for j in data:
-        if j not in lis:
-            return False
-    return True
+    count = 0
+    for i in data:
+        if count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                count = 1
+            elif i >> 4 == 0b1110:
+                count = 2
+            elif i >> 3 == 0b11110:
+                count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            count -= 1
+    return count == 0
