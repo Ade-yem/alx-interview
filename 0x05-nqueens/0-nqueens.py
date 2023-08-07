6134d6c61f435973fb502190a2f7c6803fa4cef1
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-07 06:54:48 +0100

    pycodestyle

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,12 +3,14 @@
 
 import sys
 
+
 def is_safe(board, row, col, N):
     # Check if there is a queen in the same column
     for i in range(row):
         if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
     return True
+
 
 def solve_nqueens(N):
     if N < 4:
@@ -19,7 +21,7 @@ def solve_nqueens(N):
         if row == N:
             print([[i, board[i]] for i in range(N)])
             return
-        
+
         for col in range(N):
             if is_safe(board, row, col, N):
                 board[row] = col
@@ -28,15 +30,16 @@ def solve_nqueens(N):
     board = [-1 for _ in range(N)]
     backtrack(0)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     solve_nqueens(N)
