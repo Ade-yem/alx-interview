4dc1bff1ab655c9785be665c3e470fec78023ce1
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-10 01:20:21 +0100

    style fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,9 +6,9 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
-    if (err) {
-        console.error(err)
-    }
+  if (err) {
+    console.error(err)
+  }
   const data = JSON.parse(body);
   const characters = data.characters;
   const promises = characters.map((character) => {
