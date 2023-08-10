280283de09d298b1cde976d8ae8d96f2873e064d
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-10 01:19:21 +0100

    working properly

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,15 +2,33 @@
 
 // Star Wars Characters
 
-const request = require('request')
-id = process.argv[2]
-url = `https://swapi-api.alx-tools.com/api/films/${id}`
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, res, body) => {
-    data = JSON.parse(body)
-    characters = data["characters"]
-    for (const character of characters) {
-        request(character, (error, resp, bodyy) => {
-            console.log(JSON.parse(bodyy)["name"])
-        })
+    if (err) {
+        console.error(err)
     }
-})
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  const promises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, resp, bodd) => {
+        if (error) {
+          console.error(error);
+          reject(error);
+          return;
+        }
+        const dat = JSON.parse(bodd);
+        resolve(dat.name);
+      });
+    });
+  });
+  Promise.all(promises)
+    .then((names) => {
+      names.forEach((name) => console.log(name));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
