bfb3fb0c3f38a3e89c746480a5bccc2e5e24a433
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-08-24 03:26:47 +0100

    making change correctly

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,13 +5,16 @@
 def makeChange(coins, total):
     count = 0
     coins.sort(reverse=True)
-    index = 0
-    while total > 0 and index < len(coins) - 1:
-        if total - coins[index] >= 0:
-            total -= coins[index]
-            count += 1
-        else:
-            index += 1
-        if total == 0:
-            return count
-    return -1
+    sum = 0
+    if total <= 0:
+        return 0
+    while sum < total:
+        for i in coins:
+            if sum + i <= total:
+                sum += i
+                count += 1
+                break
+            if i == coins[-1] and sum + i > total:
+                return -1
+    return count
+
